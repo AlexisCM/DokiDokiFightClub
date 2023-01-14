5aff969fd2c8cc5a8e45ce48c7313cfd81485e24
acm <alexis_cm@live.com>
2023-01-13 23:08:43 -0800

    Successfully make call to fitbit api and store access token from response

--- Assets/Code/Scripts/Fitbit/FitbitApi.cs
+++ Assets/Code/Scripts/Fitbit/FitbitApi.cs
@@ -1,21 +1,26 @@
 using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class FitbitApi : MonoBehaviour
 {
-    private const string _consumerSecret = "";
+    private const string _clientSecret = "";
     private const string _clientId = "";
 
-    // URIs
+    // URLs
     private const string _tokenUrl = "https://api.fitbit.com/oauth2/token";
-    private const string _oAuth2Uri = "https://www.fitbit.com/oauth2/authorize?";
+    private const string _oAuth2Url = "https://www.fitbit.com/oauth2/authorize?";   // Url to request user auth from Fitbit API
+    private const string _baseGetUrl = "https://api.fitbit.com/1/user/-/";  // Base Url to make GET requests to Fitbit API
+    private const string _profileUrl = _baseGetUrl + "profile.json/";       // Url to access the player's fitbit profile data
     private const string _callbackUrl = "https%3A%2F%2Falexiscm.github.io%2FDokiDokiFightClub%2Fauth%2Fddfc-game%2F";
 
     // URI arguments
-    private const string _codeChallenge = "";
     private const string _codeChallMethod = "S256";
+    // Proof of Key for Code Exchance (PKCE) Code Verifier:
+    private const string _codeVerifier = "";
+    private const string _codeChallenge = ""; // Base64-encoded SHA-256 transformation of Code Verifier
     private const string _state = "";
     private const string _scopeParams = "heartrate+profile+sleep";
 
@@ -23,6 +28,8 @@ public class FitbitApi : MonoBehaviour
     private string _returnCode; // The code returned from API after successful call
 
     private UnityWebRequest _request;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -47,8 +54,6 @@ public class FitbitApi : MonoBehaviour
         {
             AuthorizeFitbitUser();
         }
-
-        StartCoroutine(UseReturnCode());
     }
 
     public void UseRefreshToken()
@@ -58,39 +63,64 @@ public class FitbitApi : MonoBehaviour
 
     public void AuthorizeFitbitUser()
     {
-        var authorizationUrl = $"{_oAuth2Uri}&client_id={_clientId}&response_type=code&code_challenge={_codeChallenge}&" +
+        var authorizationUrl = $"{_oAuth2Url}&client_id={_clientId}&response_type=code&code_challenge={_codeChallenge}&" +
             $"code_challenge_method={_codeChallMethod}&state={_state}&scope={_scopeParams}&redirect_uri={_callbackUrl}";
+        //var authorizationUrl = $"{_oAuth2Uri}&client_id={_clientId}&response_type=code&scope={_scopeParams}&redirect_uri={_callbackUrl}";
 
         Application.OpenURL(authorizationUrl);
     }
 
-    private IEnumerator UseReturnCode()
+    public void GetData()
     {
-        var plainTextBytesToken = System.Text.Encoding.UTF8.GetBytes($"{_clientId}:{_consumerSecret}");
-        var encodedToken = Convert.ToBase64String(plainTextBytesToken);
+    }
 
+    public void UseReturnCode()
+    {
+        // Get user-inputted return code from input field
+        GameObject returnCodeObj = GameObject.Find("Canvas/CodeEntryField");
+        TMP_InputField returnCodeInput = returnCodeObj.GetComponent<TMP_InputField>();
+        _returnCode = returnCodeInput.text.ToString();
+
+        // Create request body
         var form = new WWWForm();
         form.AddField("client_id", _clientId);
         form.AddField("grant_type", "authorization_code");
         form.AddField("redirect_uri", UnityWebRequest.UnEscapeURL(_callbackUrl));
+        form.AddField("code_verifier", _codeVerifier);
         form.AddField("code", _returnCode);
 
-        var headers = form.headers;
-        headers["Authorization"] = "Basic " + encodedToken;
-        headers["Content-Type"] = "application/x-www-form-urlencoded";
+        // Create request obj and set headers
+        var request = UnityWebRequest.Post(_tokenUrl, form);
+        request.SetRequestHeader("authorization", "Basic " + EncodeBasicToken());
 
-        using (_request = UnityWebRequest.Post(_tokenUrl, form))
+        StartCoroutine(WaitForRequest(request));
+
+        // TODO: Handle access/refresh token, get user data.
+    }
+
+    IEnumerator WaitForRequest(UnityWebRequest req)
+    {
+        yield return req.SendWebRequest();
+
+        if (req.result != UnityWebRequest.Result.Success)
         {
-            yield return _request.SendWebRequest();
-
-            if (_request.result != UnityWebRequest.Result.Success)
-            {
-                Debug.Log(_request.error);
-            }
-            else
-            {
-                Debug.Log("Form upload complete!");
-            }
+            Debug.Log($"REQUEST FAILED: {req.error}\n{req.downloadHandler.text}");
         }
+        else
+        {
+            OAuth2AccessToken accessToken = JsonUtility.FromJson<OAuth2AccessToken>(req.downloadHandler.text);
+            Debug.Log(accessToken.ToString());
+        }
+    }
+
+    /// <summary>
+    /// Returns a Base64 encoded string of the application's client id and secret concatenated with a colon.
+    /// Decoded version is "client_id:client secret".
+    /// </summary>
+    /// <returns></returns>
+    private string EncodeBasicToken()
+    {
+        byte[] plainTextBytesToken = System.Text.Encoding.UTF8.GetBytes($"{_clientId}:{_clientSecret}");
+        return Convert.ToBase64String(plainTextBytesToken);
     }
 }
