98c85c8c8310df1d9b111405d894523436c02a5a
acm <alexis_cm@live.com>
2023-04-06 21:56:27 -0700

    sync player damage across network

--- Assets/Code/Scripts/Player/HealthMetre.cs
+++ Assets/Code/Scripts/Player/HealthMetre.cs
@@ -1,21 +1,58 @@
 using Mirror;
 using UnityEngine;
 
-public class HealthMetre : NetworkBehaviour
+namespace DokiDokiFightClub
 {
-    [SyncVar]
-    public int CurrentHealth;
-    public int MaxHealth;
-
-    private void Start()
+    public class HealthMetre : NetworkBehaviour
     {
-        MaxHealth = 100;
-        CurrentHealth = MaxHealth;
+        private const int _maxHealth = 100;
+
+        [SyncVar(hook = nameof(HandleHealthUpdated))]
+        private int _health;
+
+        // Delegate to update subscribers and handle behaviour when health reaches zero
+        public delegate void HealthZeroHandler();
+        public event HealthZeroHandler OnHealthZero;
+
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+            _health = _maxHealth;
+        }
+
+        private void HandleHealthUpdated(int oldValue, int newValue)
+        {
+            // TODO: Handle health UI
+
+            if (isLocalPlayer)
+                Debug.Log($"New health value: {newValue}");
+        }
+
+        //[Server]
+        //public void Add(int value)
+        //{
+        //    // Ensure negative values cannot be assigned
+        //    value = Mathf.Max(value, 0);
+        //    // Ensure new health value cannot exceed Max Health
+        //    _health = Mathf.Min(_health + value, _maxHealth);
+        //}
+
+        [Server]
+        public void Remove(int value)
+        {
+            // Ensure health cannot become negative
+            value = Mathf.Max(value, 0);
+            _health = Mathf.Max(_health - value, 0);
+
+            if (_health == 0)
+                OnHealthZero?.Invoke();
+        }
+
+        [Server]
+        public void Reset()
+        {
+            _health = _maxHealth;
+        }
     }
 
-    public void Reset()
-    {
-        CurrentHealth = MaxHealth;
-        // TODO: Update Player Health UI
-    }
 }
